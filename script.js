abb4c299d4754bd8d25f0b98694310266bbdbdf7
osivff <oscar.fitchfierro@gmail.com>
2025-05-19 12:30:01 -0700

    Add task to the list and clear input value for new task

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 const addButton = document.querySelector('button');
-const taskBox = document.querySelector('added-task-box');
+const taskBox = document.querySelector('.added-task');
 const toAdd = document.querySelector('#to-add');
+const container = document.querySelector('.container');
+
+container.addEventListener('click', (event) => {
+        const newBox = document.createElement('li');
+        const deleteBtn = document.createElement('button');
+        const editBtn = document.createElement('button');
+        const boxBtn = document.createElement('div');
+
+        deleteBtn.id = 'delete-button';
+        editBtn.id = 'edit-button';
+        newBox.id = 'list-box';
+        boxBtn.id = 'box-button'
+    
+    if(event.target.className === 'add-task-btn'){
+
+        deleteBtn.innerHTML = 'Delete Task';
+        editBtn.innerHTML = 'Edit task';
+
+        if(toAdd.value != ''){
+            newBox.textContent = `${toAdd.value}`;
+
+            boxBtn.appendChild(deleteBtn);
+            boxBtn.appendChild(editBtn);
+
+            newBox.appendChild(boxBtn);
+            taskBox.appendChild(newBox);
+        }
+        toAdd.value = '';
+    }
+});
+
+
+
 
